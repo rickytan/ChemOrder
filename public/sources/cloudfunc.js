b49cb183ccf305fd9e8e0e72d2d15fbf062f11ab
rickytan <ricky.tan.xin@gmail.com>
2013-12-21 14:22:15 +0800

    cloud

--- public/sources/cloudfunc.js
+++ public/sources/cloudfunc.js
@@ -6,17 +6,30 @@
 
 var AVCloud = AVCloud || {};
 
-(function(){
-    AVCloud.addEvent = function(title, start, end) {
-        
+(function() {
+    AVCloud.addEvent = function(title, start, end, callback) {
+
     };
-    AVCloud.delEvent = function(eventid) {
-        
+    AVCloud.delEvent = function(eventid, callback) {
+
     };
-    AVCloud.queryEvent = function(title) {
-        
+    AVCloud.queryEvent = function(title, callback) {
+
     };
-    AVCloud.listEvent = function() {
-        
+    AVCloud.loadAllEvents = function(callback) {
+
+    };
+    AVCloud.loadEvents = function(start, end, callback) {
+        if (start === null)
+            start = new Date(0);
+        if (end === null)
+            end = new Date(3000, 0, 1);
+        callback([
+            {start_date: "2013-12-18 09:00", end_date: "2013-12-18 12:00", text: "FT", subject: 'english'},
+            {start_date: "2013-12-20 10:00", end_date: "2013-12-21 16:00", text: "仪器A", subject: 'math'},
+            {start_date: "2013-12-21 10:00", end_date: "2013-12-21 14:00", text: "仪器B", subject: 'science'},
+            {start_date: "2013-12-23 16:00", end_date: "2013-12-23 17:00", text: "仪器A", subject: 'english'},
+            {start_date: "2013-12-24 09:00", end_date: "2013-12-24 17:00", text: "FT"}
+        ]);
     };
 })();
