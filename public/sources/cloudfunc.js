ef312aca64e98558d8a95506e48d507591781cf8
rickytan <ricky.tan.xin@gmail.com>
2013-12-21 13:40:04 +0800

    locale cn

---
+++ public/sources/cloudfunc.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var AVCloud = AVCloud || {};
+
+(function(){
+    AVCloud.addEvent = function(title, start, end) {
+        
+    };
+    AVCloud.delEvent = function(eventid) {
+        
+    };
+    AVCloud.queryEvent = function(title) {
+        
+    };
+    AVCloud.listEvent = function() {
+        
+    };
+})();
