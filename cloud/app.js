646d28b1a3aec3d350bbc772342127821ccb3b21
rickytan <ricky.tan.xin@gmail.com>
2013-12-23 14:41:53 +0800

    session

--- cloud/app.js
+++ cloud/app.js
@@ -7,7 +7,7 @@ app.set('views','cloud/views');   //设置模板目录
 app.set('view engine', 'ejs');    // 设置template引擎
 app.use(express.bodyParser());    // 读取请求body的中间件
 app.use(express.cookieParser("mvoiwnaksdjijwf9823flkjasijdfae"));
-app.use(avosExpressCookieSession({cookie:{maxAge:3600}}));
+//app.use(avosExpressCookieSession({cookie:{maxAge:3600}}));
 
 //使用express路由API服务/hello的http GET请求
 app.get('/hello', function(req, res) {
